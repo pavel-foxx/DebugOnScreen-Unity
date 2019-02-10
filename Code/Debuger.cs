d781962fdb133748c9b3832bc834b3b5483649ba
Quonce <quonce@mail.ru>
2019-02-10 07:06:57 +0300

    Добавлен LogError, поддержка лога объектов

--- Code/Debuger.cs
+++ Code/Debuger.cs
@@ -20,8 +20,10 @@ namespace DebugOnScreen
         private static string canvasInstanceInResources = "Debuger/DebugerCanvas";
         private static string messageInstanceInResources = "Debuger/Message";
 
-        public static void Log(string text)
+        public static void Log(object text)
         {
+            Debug.Log(text);
+
             if (instance == null)
                 _instance = Instantiate(Resources.Load<Debuger>(canvasInstanceInResources));
 
@@ -29,7 +31,22 @@ namespace DebugOnScreen
                 instance.messagesHolder = instance.transform.GetChild(0);
 
             Text message = Instantiate(Resources.Load<Text>(messageInstanceInResources), _instance.messagesHolder);
-            message.text = text;
+            message.text = text.ToString();
+        }
+
+        public static void LogError(object text)
+        {
+            Debug.LogError(text);
+
+            if (instance == null)
+                _instance = Instantiate(Resources.Load<Debuger>(canvasInstanceInResources));
+
+            if (instance.messagesHolder == null)
+                instance.messagesHolder = instance.transform.GetChild(0);
+
+            Text message = Instantiate(Resources.Load<Text>(messageInstanceInResources), _instance.messagesHolder);
+            message.color = new Color(255, 0, 0, 255);
+            message.text = text.ToString();
         }
     }
 }
